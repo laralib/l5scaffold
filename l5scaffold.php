a45e4f0dc5f66836912983c9b48d72fedbe13cfd
Martin Dos Santos <plateadodev@gmail.com>
2016-06-25 22:15:27 -0300

    Added Internalization

---
+++ l5scaffold.php
@@ -0,0 +1,21 @@
+<?php
+/*
+    This file should be on your_app_dir/resources/lang/##/ (## is your app language).
+*/
+return [
+    'create'                => 'Create',
+    'edit'                  => 'Edit',
+    'save'                  => 'Save',
+    'input_errors'          => 'There were some problems with your input.',
+    'options'               => 'OPTIONS',
+    'view'                  => 'View',
+    'empty'                 => 'No records.',
+    'id'                    => 'ID',
+    'delete'                => 'Delete',
+    'delete_confirmation'   => 'Delete? Are you sure?',
+    'back'                  => 'Back',
+    'item_created'          => 'Item created successfully.',
+    'item_updated'          => 'Item updated successfully.',
+    'item_deleted'          => 'Item deleted successfully.',
+
+];
